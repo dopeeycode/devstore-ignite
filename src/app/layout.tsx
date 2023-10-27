207b21cdb1cbd14eeb48621baa5a92d93d0133ea
Italo <italodopeey@gmail.com>
2023-10-26 23:01:05 -0300

    chore: initialize font on page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={spaceGroteskFont.variable}>{children}</body>
+    <html className={spaceGroteskFont.variable} lang="en">
+      <body>{children}</body>
     </html>
   )
 }
